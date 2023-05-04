4fa14186ad7d94598e37bf144be1154b56ba8284
Khadija-Batool <khadijagardezi10@gmail.com>
2023-05-04 18:44:13 +0500

    Movix Web App

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Movix is a web app designed for movie and TV show enthusiasts. The app
+          allows users to view trending movies and TV shows and provides
+          detailed information about them, including trailers. The app is
+          updated daily to ensure that users have access to the most up-to-date
+          information about their favorite shows and movies. With Movix, users
+          can easily stay on top of the latest entertainment trends and make
+          informed decisions about what to watch.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
